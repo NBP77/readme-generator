1445c22b6f4be95b6b4a795fd97c50c7acbdbac4
NBP77 <nbp77@outlook.com>
2021-03-25 09:30:12 -0400

    Adding examples of code from class that might help me

---
+++ index.js
@@ -0,0 +1,43 @@
+// console.log("Hello World");
+
+console.log(process.argv);
+
+console.log(process.argv[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+// // music should be an object with title, artist, and album properties
+// const music = {
+//   title: "Icey",
+//   artist: "Young Thug",
+//   album: "On the RVN"
+//   // code here
+// };
+
+// // Write code between the <div> tags to output the data from the music object above.
+// // Use an h2 element for the title and a p element for artist and title
+// const songSnippet = `<div class="song">
+// <h1> Song title is: ${music.title}<h1>
+// <h2> The artist is: ${music.artist}<h2> 
+// <h3> The album is: "${music.album}"<h3>
+// </div>`;
+
+// const element = document.getElementById("music");
+// element.innerHTML = songSnippet;
+
+// // musicStyle = document.querySelector("music");
+// document.querySelector("music").style.color = "green";
+
+
+// // const greeting = `My name is ${arya.first}!
+// // I am loyal to ${arya.allegiance}.
+// // I come from ${arya.origin}!`;
