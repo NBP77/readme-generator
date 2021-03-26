70059dc509e941fe1fd847e7da28d3a69d611d76
NBP77 <nbp77@outlook.com>
2021-03-26 08:01:03 -0400

    Getting my page set up and need to install inquirer

--- index.js
+++ index.js
@@ -1,19 +1,110 @@
-// console.log("Hello World");
+// node imports
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-console.log(process.argv);
-
-console.log(process.argv[2]);
-
-
-
-
-
-
-
-
-
+// Questions for the user
+inquirer.prompt()
+    [
+      {
+        type: 'input',
+        message= "Whats the project name?",
+        name: 'Title',
+        validate
+      }
+    ]
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // console.log("Hello World");
+
+// console.log(process.argv);
+
+// console.log(process.argv[2]);
+
+
+
+
+
+
+// const inquirer = require("inquirer");
+// const fs = require("fs");
+// inquirer
+//   .prompt([
+//     {
+//       type: "input",
+//       message: "What is your name?",
+//       name: "username",
+//     },
+//         {
+//       type: "checkbox",
+//       message: "What languages do you know?",
+//       name: "foreignLanguages",
+//             choices: ["Spanish", "German", "French", "ASL"]
+//     },
+//         {
+//       type: "list",
+//       message: "What is your preferred method of communication?",
+//             name: "contact",
+//       choices: ["text", "call", "DM", "carrier pigeon"]
+//     },
+  // ])
+
+  // .then((response) => {
+  //   console.log(response);
+  //       // console.log("-----------------------------------------");
+  //       // console.log(response.username);
+  //       // console.log(response.foreignLanguages[2]);
+  //       // console.log(response.contact);
+  //       const filename = `${response.username.toLowerCase().split(" ").join("")}.json`
+  //       // const filename = response.username + ".json"
+  //       fs.appendFile(filename, JSON.stringify(response, null, '\t'), (err) =>
+  //       err ? console.error(err) : console.log("created file successfully")
+  //       )
+  // });
 
 // // music should be an object with title, artist, and album properties
 // const music = {
